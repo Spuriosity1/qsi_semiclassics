8b7b2547a435dc1d470a5c198f98cc3b07435f9e
Spuriosity1 <alaric.sanders@gmail.com>
2025-09-15 15:03:53 +0200

    qsi_semiclassical clean example

--- driver/plan_static_corr.sh
+++ driver/plan_static_corr.sh
@@ -1,23 +1,52 @@
 #!/bin/bash
 
-
 PROG=../bin/qsi_static
 DIR="$HOME/out/static_20"
 
 mkdir -p $DIR
+
+
+POSITIONAL_ARGS=()
+TEMPFILE="temps"
+
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--force)
+      FORCEMKDIR="-p"
+      shift # past argument
+      ;;
+    -t|--temps)
+      TEMPFILE="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 
 INFILE=../input/SSF/medium.toml
 
 PLANFILE=static_corr.plan
 
 # Temperature points from file
-read -d '' -r -a temps < "temps"
+echo "Importing temperature points from file '${TEMPFILE}'"
+read -d '' -r -a temps < "${TEMPFILE}"
 
-echo '' > $PLANFILE
 
 for i in `seq 0 18`;
 do
-    mkdir "$DIR/$i"
+    mkdir $FORCEMKDIR "$DIR/$i"
     for j in `seq 17 32`;
     do
         echo "$PROG $INFILE \"$DIR/$i\" $((64*$j+$i)) --temperature=${temps[$i]} >\"$DIR/$i/$j.list\" 2>\"$DIR/$i/$j.track\"" >> $PLANFILE
